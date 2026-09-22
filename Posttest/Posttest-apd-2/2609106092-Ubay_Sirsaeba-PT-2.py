457ce023e148f385ffd61221e0f3cb1ea71cd1e3
Obayse <sirsaebaubay@gmail.com>
2026-09-22 23:17:24 +0800

    first commit

---
+++ Posttest/Posttest-apd-2/2609106092-Ubay_Sirsaeba-PT-2.py
@@ -0,0 +1,30 @@
+# Menentukan harga merchandise
+merchandise_1 = 45000
+merchandise_2 = 50000
+merchandise_3 = 60000
+merchandise_4 = 75000
+merchandise_5 = 90000
+merchandise_6 = 120000
+biaya_bungkus = 75000
+
+
+total_harga = merchandise_1 + merchandise_2 + merchandise_3 + merchandise_4 + merchandise_5 + merchandise_6 + biaya_bungkus
+rata_rata = total_harga / 6
+nim = 92
+bolean = nim > rata_rata
+
+print("merchandise_1 =", merchandise_1)
+print("merchandise_2 =", merchandise_2)
+print("merchandise_3 =", merchandise_3)
+print("merchandise_4 =", merchandise_4)
+print("merchandise_5 =", merchandise_5)
+print("merchandise_6 =", merchandise_6)
+print("biaya_bungkus =", biaya_bungkus)
+print("total_harga =", total_harga)
+print("rata_rata =", rata_rata)
+print("nim =", nim)
+print("bolean =", bolean)
+
+
+
+
