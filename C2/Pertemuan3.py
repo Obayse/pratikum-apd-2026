0e0a27e27136ed8ba8f3dbdad4026b4411631e29
Obayse <sirsaebaubay@gmail.com>
2026-09-24 16:29:52 +0800

    update

---
+++ C2/Pertemuan3.py
@@ -0,0 +1,34 @@
+#pratikum ="orsikom"
+
+#if pratikum == "apd" : 
+#    print("kamu lagi melihat mengikuti praktikum apd sekarang")
+#else:
+#    print("kamu mengikuti pratikum lain")
+
+#umur = int(input("masukan umur kalian : "))
+
+#if umur >= 17:
+#    print("kamu sudah legal")
+#else:
+#    print("kamu belum cukup umur")
+#
+# kendaraan = input("Masukkan jenis kendaraan anda: ").upper()
+
+# if kendaraan == "MOBIL":
+#     tarif_parkir = 10000
+# elif kendaraan == "MOTOR":
+#     tarif_parkir = 5000
+# else:
+#     tarif_parkir = 15000
+
+# print(f"Tarif parkir untuk {kendaraan} adalah: {tarif_parkir}")
+
+# umur = 20
+# status = "Dewasa" if umur >= 18 else "Belum Dewasa"
+# print(status)
+
+# umur = int(input("masukkan umur kalian : "))
+umur = 16
+status = "kamu boleh masuk" if umur >= 16 else "kamu belum cukup umur
+print(status)
+
